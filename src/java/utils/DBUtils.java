3a92c7ba72cea31751abfbc80319418b47412c15
KietPham-VN <phamanhkiet.dev@gmail.com>
2025-01-20 14:13:50 +0700

    first commit

---
+++ src/java/utils/DBUtils.java
@@ -0,0 +1,40 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class DBUtils
+{
+
+    private static final Logger LOGGER = Logger.getLogger(DBUtils.class.getName());
+    private static final Dotenv DOT_ENV = Dotenv.load();
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException
+    {
+        Connection conn = null;
+        try
+        {
+            String driver = DOT_ENV.get("DB_DRIVER");
+            String url = DOT_ENV.get("DB_URL");
+            String username = DOT_ENV.get("DB_USERNAME");
+            String password = DOT_ENV.get("DB_PASSWORD");
+
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+            LOGGER.info("Database connection successfully established.");
+        } catch (ClassNotFoundException e)
+        {
+            LOGGER.log(Level.SEVERE, "SQL Driver not found", e);
+            throw e;
+        } catch (SQLException e)
+        {
+            LOGGER.log(Level.SEVERE, "Failed to create database connection", e);
+            throw e;
+        }
+        return conn;
+    }
+}
