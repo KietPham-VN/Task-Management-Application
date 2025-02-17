d26db8c47c61f6ade5b0a9f0a05016658f1d0005
KietPham-VN <phamanhkiet.dev@gmail.com>
2025-02-17 21:15:01 +0700

    test gitignore

--- src/java/common/constants/Pages.java
+++ src/java/common/constants/Pages.java
@@ -5,4 +5,5 @@ public class Pages
     public static String LOGIN = "views/login.jsp";
     public static String REGISTER = "views/register.jsp";
     public static String HOME = "views/index.jsp";
+    public static String VIEW_MEMBER_PROJECTS = "views/team-member/view-member-projects.jsp";
 }
